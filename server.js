25807afdbd876992190ea51726c74664010d0345
Zonoik <jonaneiro09@gmail.com>
2025-02-28 11:27:31 +0100

    hot fix

--- server.js
+++ server.js
@@ -9,16 +9,26 @@ const PORT = process.env.PORT || 3000;
 // Middleware para servir imágenes estáticas
 app.use('/images', express.static(path.join(__dirname, 'public', 'images')));
 
-// Middleware para el resto
+// Middleware para manejar CORS y JSON
 app.use(cors());
 app.use(express.json());
+
+// Verificar si se ha enviado algo en el cuerpo de la solicitud
+const checkBody = (req, res, next) => {
+  if (Object.keys(req.body).length === 0) {
+    console.log('Solicitud sin cuerpo');
+    return res.status(400).json({ error: 'No se ha enviado ningún dato en el cuerpo de la solicitud' });
+  }
+  console.log('Cuerpo de la solicitud:', req.body);
+  next();
+};
 
 // Cargar datos desde el archivo JSON
 const data = JSON.parse(fs.readFileSync('one_piece_characters.json', 'utf-8'));
 const { groups, members } = data; // Declaramos `groups` y `members` como constantes.
 
 // Endpoint POST para obtener los miembros de un grupo
-app.post('/api/members', (req, res) => {
+app.post('/api/members', checkBody, (req, res) => {
   const { group } = req.body;
 
   if (!members[group]) {
@@ -29,7 +39,7 @@ app.post('/api/members', (req, res) => {
 });
 
 // Endpoint POST para obtener información de un miembro específico
-app.post('/api/members/item', (req, res) => {
+app.post('/api/members/item', checkBody, (req, res) => {
   const { name } = req.body;
 
   for (const group in members) {
@@ -43,7 +53,7 @@ app.post('/api/members/item', (req, res) => {
 });
 
 // Endpoint POST para buscar ítems según criterios
-app.post('/api/search', (req, res) => {
+app.post('/api/search', checkBody, (req, res) => {
   const { query } = req.body;
 
   const results = [];
@@ -56,14 +66,13 @@ app.post('/api/search', (req, res) => {
   res.json(results);
 });
 
-// Endpoint POST para obtener los grupos
+// Endpoint POST para obtener los grupos (sin necesidad de cuerpo)
 app.post('/api/groups', (req, res) => {
   res.json(groups);
 });
 
-
 // Endpoint POST para obtener los miembros de un grupo específico
-app.post('/api/members/group', (req, res) => {
+app.post('/api/members/group', checkBody, (req, res) => {
   const { group } = req.body;
 
   if (members[group]) {
@@ -72,7 +81,6 @@ app.post('/api/members/group', (req, res) => {
     res.status(404).json({ error: 'Grupo no encontrado' });
   }
 });
-
 
 // Iniciar el servidor
 app.listen(PORT, () => {
